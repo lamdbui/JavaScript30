1e5a1b98cecc9b36d8876ca73571f46c966e2ffe
Lam Bui <lamdbui@gmail.com>
2018-03-23 15:09:01 -0700

    Add red pixel hue filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -30,6 +30,10 @@ function paintToCanvas() {
   // capture image
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // do some filtering
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -46,6 +50,17 @@ function takePhoto() {
   link.textContent = 'Download Image';
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] += 100;   // r
+    pixels.data[i + 1] -= 50;    // g
+    pixels.data[i + 2] *= 0.5;   // b
+    // pixels.data[i + 3] // a
+
+  }
+  return pixels;
 }
 
 video.addEventListener('canplay', paintToCanvas);
