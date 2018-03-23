b3491e594abf8ab665d89a85a2470410657ef6c7
Lam Bui <lamdbui@gmail.com>
2018-03-23 15:12:03 -0700

    Add RGB split filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -32,7 +32,8 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
     // do some filtering
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
@@ -50,6 +51,17 @@ function takePhoto() {
   link.textContent = 'Download Image';
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];    // r
+    pixels.data[i + 100] = pixels.data[i + 1];    // g
+    pixels.data[i - 150] = pixels.data[i + 2];    // b
+    // pixels.data[i + 3] // a
+
+  }
+  return pixels;
 }
 
 function redEffect(pixels) {
